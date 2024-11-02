3e104682c9d3f99ca011b38118ce6fa4c6155bfd
morteza Ebrahimi <morteza.nsrn@gmail.com>
2024-11-02 12:09:31 +0330

    feat: add logo in header; logo and backgroundColor

--- rest-country/src/App.jsx
+++ rest-country/src/App.jsx
@@ -1,9 +1,16 @@
+import './app.css'
 
 function App() {
 
   return (
     <>
-      
+      <header className="header">
+        <div className="container">
+          <h5>Where in the world </h5>
+        </div>
+      </header>
+      <div className="container">
+      </div>
     </>
   )
 }
